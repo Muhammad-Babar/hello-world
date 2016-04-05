5b87aa121f16bcad45e4e17545eaa742e655c61c
Muhammad Baber <muhammad.baber@square63.com>
2016-04-05 23:36:21 +0500

    Order management: Created new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ordermanagement::Application.config.secret_token = '786c57c7873758e00ff84a7ab68f2048cd5423f659eb710b6f4e6baee898e14f7cb177a80204e53e80a98c0194b0df6ceb7926d0370061612a27a0671eefbb44'
