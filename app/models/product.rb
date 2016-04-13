afa679f68f360d8b58a7e14afb632e4a7a81d0a8
Muhammad Baber <muhammad.baber@square63.com>
2016-04-13 18:37:58 +0500

    Product: Added attr_accessible, and atter_accessor

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price
+  attr_accessible :name, :price, :term
   belongs_to :user
+  attr_accessor :term
+  validates :name, presence: true, length: {minimum: 4}
+  validates :price, presence: true
+  # before_create :blah
+  after_create :normalize_name
+  private
+      def checkresult
+      p "Invoked"
+    end
+    def normalize_name
+      self.name = self.name.downcase.titleize
+    end
 end
